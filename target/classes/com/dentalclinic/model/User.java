e555d1e405c5165130adaca813db5346a13ceaee
Zaina Azwer <zainaazwer@gmail.com>
2026-07-28 21:04:45 +0530

    Added User Class

--- target/classes/com/dentalclinic/model/User.java
+++ target/classes/com/dentalclinic/model/User.java
@@ -11,7 +11,6 @@ public class User implements Serializable {
     private String fullName;
     private String email;
     private String role;
-    private String createdAt;
 
     public User() {}
 
@@ -25,24 +24,52 @@ public class User implements Serializable {
     }
 
     // Getters and Setters
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
+    public int getUserId(){
+    	return userId;
+    }
     
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public void setUserId(int userId){
+    	this.userId = userId;
+    }
     
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getUsername(){
+    	return username;
+    }
     
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setUsername(String username){
+    	this.username = username;
+    }
     
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getPassword(){
+    	return password;
+    }
     
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public void setPassword(String password){
+    	this.password = password; 
+    }
     
-    public String getCreatedAt() { return createdAt; }
-    public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
+    public String getFullName(){
+    	return fullName; 
+    }
+    
+    public void setFullName(String fullName){
+    	this.fullName = fullName;
+    }
+    
+    public String getEmail(){
+    	return email;
+    }
+    
+    public void setEmail(String email){
+    	this.email = email;
+    }
+    
+    public String getRole(){
+    	return role;
+    }
+    
+    public void setRole(String role){
+    	this.role = role;
+    }
+    
 }
