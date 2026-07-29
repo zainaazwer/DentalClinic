f528feac4c04ac70da58366f474f3164e2929a11
Zaina Azwer <zainaazwer@gmail.com>
2026-07-29 18:56:04 +0530

    Added Database Connection Class

---
+++ src/main/java/com/dentalclinic/dao/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package com.dentalclinic.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/?user=root";
+    private static final String USER = "root";
+    private static final String PASSWORD = "sql1";
+    
+    private static Connection connection = null;
+    
+    private DatabaseConnection() {}
+    
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC Driver not found", e);
+            }
+        }
+        return connection;
+    }
+    
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
